81d9dd06b5a6dbb82fdd3b0ac329892b31bde29e
alfred <alfred@klandestino.se>
2014-12-18 19:04:00 +0000

    hide cursor, since it creates an ugly black box in top left corner i X11, for some stupid reason

--- features/quiet/post-install.sh
+++ features/quiet/post-install.sh
@@ -5,7 +5,7 @@ cp splash1.png work/etc/splash1.png
 cp splash2.png work/etc/splash2.png
 cp splash3.png work/etc/splash3.png
 
-echo 'logo.nologo dwc_otg.lpm_enable=0 console=tty2 quiet root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait' >workboot/cmdline.txt
+echo 'logo.nologo vt.global_cursor_default=0 dwc_otg.lpm_enable=0 console=tty2 quiet root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait' >workboot/cmdline.txt
 
-sed -i 's/1:.* tty1//' boot/etc/inittab
+sed -i 's/1:.* tty1//' work/etc/inittab
 
