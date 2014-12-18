242037213b562b13784f3ad5cf2a2870c3ce268d
alfred <alfred@klandestino.se>
2014-12-18 01:48:08 +0000

    dont hide the cursor

--- features/quiet/post-install.sh
+++ features/quiet/post-install.sh
@@ -5,5 +5,5 @@ cp splash1.png work/etc/splash1.png
 cp splash2.png work/etc/splash2.png
 cp splash3.png work/etc/splash3.png
 
-echo 'logo.nologo vt.global_cursor_default=0 dwc_otg.lpm_enable=0 console=tty2 quiet root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait' >workboot/cmdline.txt
+echo 'logo.nologo dwc_otg.lpm_enable=0 console=tty2 quiet root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait' >workboot/cmdline.txt
 
